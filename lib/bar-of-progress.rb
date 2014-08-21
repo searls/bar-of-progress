c9b9443fe42dc70d4de4d41a60aac6557951d919
Justin Searls <searls@gmail.com>
2014-08-20 22:55:59 -0400

    Encode all the defaults

--- lib/bar-of-progress.rb
+++ lib/bar-of-progress.rb
@@ -1,12 +1,21 @@
 require "bar_of_progress/version"
 
 class BarOfProgress
-  def initialize
-    @total = 100.0
+  DEFAULTS = {
+    :total => 100.0,
+    :length => 10,
+    :braces => %w{[ ]},
+    :complete_indicator => "●",
+    :partial_indicator => "◍",
+    :incomplete_indicator => "◌"
+  }
+
+  def initialize(options = {})
+    @options = DEFAULTS.merge(options)
   end
 
   def progress(amount = 0)
-    bubbles = betwixt(((amount / @total) * 10), 0, 10)
+    bubbles = betwixt(((amount / @options[:total]) * 10), 0, 10)
     full_bubbles = bubbles.floor
     partial_bubbles = bubbles % 1 == 0 ? 0 : 1
     "[#{chars('●', full_bubbles)}#{chars('◍', partial_bubbles)}#{chars('◌', (10 - full_bubbles - partial_bubbles))}]"
