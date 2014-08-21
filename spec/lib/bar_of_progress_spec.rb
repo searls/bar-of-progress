58ae5c00e5ce45b2acfe141391e7956dd6ab3c65
Justin Searls <searls@gmail.com>
2014-08-20 22:10:11 -0400

    fourth failing test

--- spec/lib/bar_of_progress_spec.rb
+++ spec/lib/bar_of_progress_spec.rb
@@ -7,6 +7,7 @@ describe BarOfProgress do
     Then { subject.progress(-1) == "[◌◌◌◌◌◌◌◌◌◌]" }
     Then { subject.progress == "[◌◌◌◌◌◌◌◌◌◌]" }
     Then { subject.progress(0) == "[◌◌◌◌◌◌◌◌◌◌]" }
+    Then { subject.progress(49) == "[●●●●◍◌◌◌◌◌]" }
     Then { subject.progress(50) == "[●●●●●◌◌◌◌◌]" }
     Then { subject.progress(100) == "[●●●●●●●●●●]" }
     Then { subject.progress(101) == "[●●●●●●●●●●]" }
