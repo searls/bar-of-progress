36507e8f17a1d8b9964c807c762486577eb5beaf
Justin Searls <searls@gmail.com>
2014-08-20 22:18:33 -0400

    Fix failing boundary tests by applying boundaries

--- lib/bar_of_progress.rb
+++ lib/bar_of_progress.rb
@@ -6,9 +6,21 @@ class BarOfProgress
   end
 
   def progress(amount = 0)
-    bubbles = ((amount / @complete) * 10)
+    bubbles = betwixt(((amount / @complete) * 10), 0, 10)
     full_bubbles = bubbles.floor
     partial_bubbles = bubbles % 1 == 0 ? 0 : 1
-    "[#{'●' * full_bubbles}#{'◍' * partial_bubbles}#{'◌' * (10 - full_bubbles - partial_bubbles)}]"
+    "[#{chars('●', full_bubbles)}#{chars('◍', partial_bubbles)}#{chars('◌', (10 - full_bubbles - partial_bubbles))}]"
+  end
+
+private
+
+  # This method is amazing.
+  def betwixt(n, min, max)
+    [[n, min].max, max].min
+  end
+
+  def chars(char, n)
+    return "" if n <= 0
+    char * n
   end
 end
