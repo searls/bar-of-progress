51b0bd0c206680e5b8af5f5b5816747bd02f7e70
Justin Searls <searls@gmail.com>
2014-08-20 22:21:56 -0400

    rename file so gem can be loaded with dashes

--- spec/lib/bar_of_progress_spec.rb
+++ spec/lib/bar_of_progress_spec.rb
@@ -1,4 +1,4 @@
-require 'bar_of_progress'
+require 'bar-of-progress'
 
 describe BarOfProgress do
   context "vanilla default progress bar" do
@@ -11,6 +11,5 @@ describe BarOfProgress do
     Then { subject.progress(50) == "[●●●●●◌◌◌◌◌]" }
     Then { subject.progress(100) == "[●●●●●●●●●●]" }
     Then { subject.progress(101) == "[●●●●●●●●●●]" }
-
   end
 end
