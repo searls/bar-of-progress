39db266dffc25a8a6d50a0247d2d7ce42781f41b
Justin Searls <searls@gmail.com>
2014-08-20 22:01:37 -0400

    Refactor tests

--- spec/lib/bar_of_progress_spec.rb
+++ spec/lib/bar_of_progress_spec.rb
@@ -4,20 +4,9 @@ describe BarOfProgress do
   context "vanilla default progress bar" do
     subject { BarOfProgress.new }
 
+    Then { subject.progress == "[◌◌◌◌◌◌◌◌◌◌]" }
+    Then { subject.progress(50) == "[●●●●●◌◌◌◌◌]" }
+    Then { subject.progress(100) == "[●●●●●●●●●●]" }
 
-    describe "initial progress" do
-      When(:result) { subject.progress }
-      Then { result == "[◌◌◌◌◌◌◌◌◌◌]" }
-    end
-
-    describe "100% progress" do
-      When(:result) { subject.progress(100) }
-      Then { result == "[●●●●●●●●●●]" }
-    end
-
-    describe "50% progress" do
-      When(:result) { subject.progress(50) }
-      Then { result == "[●●●●●◌◌◌◌◌]" }
-    end
   end
 end
