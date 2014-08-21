5c4937a489f68501d3907b269ecdfb0284e3f080
Justin Searls <searls@gmail.com>
2014-08-20 22:00:52 -0400

    pass third test

--- lib/bar_of_progress.rb
+++ lib/bar_of_progress.rb
@@ -1,11 +1,12 @@
 require "bar_of_progress/version"
 
 class BarOfProgress
+  def initialize
+    @complete = 100.0
+  end
+
   def progress(amount = 0)
-    if amount == 0
-      "[◌◌◌◌◌◌◌◌◌◌]"
-    else
-      "[●●●●●●●●●●]"
-    end
+    bubbles = ((amount / @complete) * 10).round
+    "[#{'●' * bubbles}#{'◌' * (10 - bubbles)}]"
   end
 end
