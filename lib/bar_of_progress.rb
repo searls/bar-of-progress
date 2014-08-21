2bcec36775020d7f0000fe908936560b2aa8ef28
Justin Searls <searls@gmail.com>
2014-08-20 21:50:40 -0400

    second passing test

--- lib/bar_of_progress.rb
+++ lib/bar_of_progress.rb
@@ -1,7 +1,11 @@
 require "bar_of_progress/version"
 
 class BarOfProgress
-  def progress
-    "[◌◌◌◌◌◌◌◌◌◌]"
+  def progress(amount = 0)
+    if amount == 0
+      "[◌◌◌◌◌◌◌◌◌◌]"
+    else
+      "[●●●●●●●●●●]"
+    end
   end
 end
