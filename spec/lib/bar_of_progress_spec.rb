c07faab591b224584ee4245daaca3577449b94c0
Justin Searls <searls@gmail.com>
2014-08-20 22:02:33 -0400

    Add tests to cover boundary cases

--- spec/lib/bar_of_progress_spec.rb
+++ spec/lib/bar_of_progress_spec.rb
@@ -4,9 +4,12 @@ describe BarOfProgress do
   context "vanilla default progress bar" do
     subject { BarOfProgress.new }
 
+    Then { subject.progress(-1) == "[◌◌◌◌◌◌◌◌◌◌]" }
     Then { subject.progress == "[◌◌◌◌◌◌◌◌◌◌]" }
+    Then { subject.progress(0) == "[◌◌◌◌◌◌◌◌◌◌]" }
     Then { subject.progress(50) == "[●●●●●◌◌◌◌◌]" }
     Then { subject.progress(100) == "[●●●●●●●●●●]" }
+    Then { subject.progress(101) == "[●●●●●●●●●●]" }
 
   end
 end
